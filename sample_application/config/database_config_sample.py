2e9a6b23988880c0c7c4cbae36d1d7e14388c3f3
Emanuele Manfredini <emanuele.manfredini.1992@gmail.com>
2024-02-12 22:33:18 +0100

    initial commit

---
+++ sample_application/config/database_config_sample.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+db_config = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': Path(__file__).resolve().parent.parent / 'db.sqlite3',
+    }
+}
